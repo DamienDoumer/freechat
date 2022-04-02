6b81880543e97cfa925e9fdc74140a8c8a7df435
Damien Doumer <damientohin@gmail.com>
2022-04-02 21:52:26 +0200

    added basic styles.

---
+++ src/Pure_MAUI/FreeChat/FreeChat/Views/Styles/Fonts.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeChat.Views.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Fonts : ResourceDictionary
+    {
+        public Fonts()
+        {
+            InitializeComponent();
+        }
+    }
+}
