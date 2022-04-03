b9c69256a2092e4f9dfb8d72c866a2e30fc7d42a
Damien Doumer <damientohin@gmail.com>
2022-04-03 11:33:46 +0200

    Corrected issues with page additions

--- src/Pure_MAUI/FreeChat/FreeChat/Views/Styles/Colors.xaml.cs
+++ src/Pure_MAUI/FreeChat/FreeChat/Views/Styles/Colors.xaml.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace FreeChat.Views.Styles
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Colors : ResourceDictionary
     {
         public Colors()
