d530eeb725c23cafe5437480037d2c0e653ec342
Damien Doumer <damientohin@gmail.com>
2022-04-03 11:19:04 +0200

    added required pages, and an appshell

---
+++ src/Pure_MAUI/FreeChat/FreeChat/Views/Pages/BaseTabbedPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace FreeChat.Views.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BaseTabbedPage : ContentPage
+    {
+        public BaseTabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Pure_MAUI/FreeChat/FreeChat/Views/Pages/ConversationsPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeChat.Views.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConversationsPage : ContentPage
+    {
+        public ConversationsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
