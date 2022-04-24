5a0884b489c039855002e792f397d8d35c859a92
Damien Doumer <damientohin@gmail.com>
2022-04-24 15:34:26 +0200

    added templates

--- src/Pure_MAUI/FreeChat/FreeChat/Views/DataTemplates/MessagePeerSentTemplate.xaml.cs
+++ src/Pure_MAUI/FreeChat/FreeChat/Views/DataTemplates/MessagePeerSentTemplate.xaml.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
 namespace FreeChat.Views.DataTemplates
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
--- src/Pure_MAUI/FreeChat/FreeChat/Views/DataTemplates/SimpleUserProfileTemplate.xaml.cs
+++ src/Pure_MAUI/FreeChat/FreeChat/Views/DataTemplates/SimpleUserProfileTemplate.xaml.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace FreeChat.Views.DataTemplates
 {
